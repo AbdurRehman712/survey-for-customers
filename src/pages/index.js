10e800b1cb0d0ef6a4f78c1ced4db00c13cfbea4
armwurfel <46182746+armwurfel@users.noreply.github.com>
2018-12-29 23:29:37 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,7 +75,7 @@ export default IndexPage;
 export const query = graphql`
   query IndexQuery {
     posts: allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "//posts/" } }
+      filter: { fileAbsolutePath: { regex: "/posts/" } }
       sort: { fields: [fields___prefix], order: DESC }
     ) {
       edges {
